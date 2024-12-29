eac33fc5cc0d5299e55ad56828842700c4bcf75e
Serigne LEYE <leyeserignembackebousso@gmail.com>
2024-12-29 14:40:25 +0000

    Modifier tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -6,10 +6,10 @@ import unittest
 
 class AppTestCase(unittest.TestCase):
 
- def test_root_text(self):
-    tester = app.test_client(self)
-    response = tester.get('/')
-    assert 'Hello world!' in response.data
+    def test_root_text(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        assert 'Hello world!' in response.data
 
 
 if __name__ == '__main__':
